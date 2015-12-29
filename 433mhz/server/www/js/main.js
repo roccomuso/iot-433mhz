d581bd6ce7baa24902030aa0a1c40a12e642b769
Rocco Musolino <rocco@hackerstribe.com>
2015-12-29 01:30:04 +0100

    On-Off big button added.

--- 433mhz/server/www/js/main.js
+++ 433mhz/server/www/js/main.js
@@ -1,36 +1,3 @@
-// Sidebar Menu
-
-var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
-
-    trigger.click(function () {
-      hamburger_cross();      
-    });
-
-    function hamburger_cross() {
-
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
-
-    $('.overlay').click(function(){ // close sidebar on overlay click
-        $('#wrapper').toggleClass('toggled');
-        hamburger_cross();
-    });
-  
-  $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-  });
 
   // Cards
 
