aa6ee9bfde58c9b817ec1788df7184fd2eb4d513
Rocco Musolino <rocco@hackerstribe.com>
2016-01-02 01:33:28 +0100

    Saving socket clients into clients Array.

--- server/components/socketFunctions.js
+++ server/components/socketFunctions.js
@@ -1,6 +1,11 @@
 
-module.exports = function(socket){
+// For socket sending methods see http://stackoverflow.com/questions/10058226/send-response-to-all-clients-except-sender-socket-io
+
+
+
+module.exports = function(socket, rf433mhz, db){
 	
+	var clients = []; // Store sockets
 	
 	// exposed methods
 	var methods = {
@@ -9,22 +14,27 @@ module.exports = function(socket){
 			// get data from DB to render the init cards on the UI.
 			return {prova: true, come: 'va'};
 		},
-		onConnection: function(user_socket){
+		onConnection: function(client_socket){ // single user socket
             // TODO ...
             console.log('User connected via socket.io');
+
+            // Save the user:
+            clients.push(client_socket); 
+		    client_socket.on('disconnect', function() {
+		        clients.splice(clients.indexOf(client_socket), 1);
+		        // clients.forEach(function(sckt){ console.log(sckt.id);} ); // remaining clients
+		    });
+
             // Sending actual configuration
             
-            user_socket.emit('initData', methods.getInitData());
+            client_socket.emit('initData', methods.getInitData());
 
+            // TODO
             // Tutti i socket.on andrebbero qui.
-            user_socket.on('example', function(data){ console.log(data); });
+            client_socket.on('example', function(data){ console.log(data); });
                 
-		},
-		codeInDB: function(code){
-			// TODO
-			// check if the given code is in DB
-
 		}
+
 	};
 
 	return methods;
