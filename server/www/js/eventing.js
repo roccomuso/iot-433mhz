2120fce6ab7f2bfbf5261eafb332c3308f70279d
Rocco Musolino <rocco@hackerstribe.com>
2016-01-14 06:26:55 +0100

    Handling card submission with dynamic codes manipulation. (TODO. Ajax call).

--- server/www/js/eventing.js
+++ server/www/js/eventing.js
@@ -6,6 +6,7 @@
 events.on('ignoreCode', function(code){
 	// send ignore's command to the Server 
 	socket.emit('ignoreCode', code); // NB. socket defined below eventing.js
+	RFcodes.deleteCode(code); // remove code from the incoming_codes.
 });
 
 events.on('removeIgnoreCode', function(code){
@@ -22,34 +23,51 @@ events.on('assignCode', function(code){
   });  
 });
 
+
 events.on('newCardClick', function(code){
 	var view = {title: 'New Card for code', code: code};
 	templating.renderTemplate('newCardForm.mustache', $('#main_modal_box'), view).then(function(){
 		$('#new-card-dialog').modal('show');
-		// no blank space admitted and all lower case.
-		$('#shortname').keyup(function(){
-		    this.value = this.value.replace(/\s+/g, '-').toLowerCase();
+		// all lower case, no blank space or non alpha numeric chars are admitted for this 2 fields
+		$('#shortname, #roomname').keyup(function(){
+		    this.value = this.value.replace(/\W+/g, " ");
+    		this.value = this.value.replace(/\s+/g, '-').toLowerCase();
 		});
 		// dynamic form adapter
-		$("#type").change(function() {
+		$('#type').change(function() {
 		    var str = '<label for="code" class="col-md-2 control-label">RF Codes</label>';
-		    $("#type option:selected").each(function() {
+		    $('#type option:selected').each(function() {
 		    	var selected = $(this).val();
 		      if(selected === 'switch'){
-		      	str += '<div class="col-md-5"><input type="number" class="form-control" name="on_code" id="code" placeholder="ON code" required/></div><div class="col-md-5"><input type="number" class="form-control" name="off_code" placeholder="OFF code" required/></div>';
+		      	str += '<div class="col-md-5"><select id="code" name="on_code" class="form-control codes" required>'+RFcodes.getCodesInHTML(code)+'</select><span class="help-block">ON Code</span></div><div class="col-md-5"><select name="off_code" class="form-control codes" required>'+RFcodes.getCodesInHTML(code)+'</select><span class="help-block">OFF Code</span></div>';
 		      }else if(selected === 'alarm'){
-		      	str += '<div class="col-md-5"><input type="number" class="form-control" name="trigger_code" id="_code" placeholder="Trigger Code" required/></div>';
+		      	str += '<div class="col-md-5"><select id="code" name="trigger_code" class="form-control codes" required>'+RFcodes.getCodesInHTML(code)+'</select><span class="help-block">Trigger Code</span></div>';
 		      }else if (selected === 'info'){ // we don't need to have codes
 		      	str = '';
 		      }
 		    });
-		    $("#type_related_content").html(str);
+		    $('#type_related_content').html(str);
 		  }).change();
+		// modal box incoming codes listener (to updated select box lists)
+		function updateSelectBoxes(code){
+			$('.codes').html(RFcodes.getCodesInHTML(code)).fadeIn(500);
+		}
+		RFcodes.on('newCode', updateSelectBoxes); // set listener
+		RFcodes.on('removedCode', updateSelectBoxes); // set listener
+		
+		// remove listeners when modal gets closed
+		$('#new-card-dialog').on('hidden.bs.modal', function (e) {
+		  RFcodes.off('newCode', updateSelectBoxes);
+		  RFcodes.off('removedCode', updateSelectBoxes);
+		  console.log('modal box closed');
+		  RFcodes.deleteCode(code);
+		});
 		// form submitting listener
 		$('#newCardForm').submit(function(e){
 			e.preventDefault();
 			// TODO ajax POST to /api/card/new
 
+			RFcodes.deleteCode(code); // remove code from the incoming codes. (in this way it can't be used anymore)
 		});
 
 	}).catch(function(err){ // err
@@ -57,25 +75,25 @@ events.on('newCardClick', function(code){
 	}); 
 });
 
-var incoming_codes = {}; // RF codes with an opened snackbar
+
 events.on('renderSnackbar', function(_code){
-	if (!incoming_codes.hasOwnProperty(_code)){ // code first time detected
-		incoming_codes[_code] = {badge_count: 1};
-		var mex = '<span class="pull-left" style="padding-top: 11px"><span class="badge">'+incoming_codes[_code].badge_count+'</span> Code detected: '+_code+'</span>'+
+	if (!RFcodes.incoming_codes.hasOwnProperty(_code)){ // code first time detected
+		RFcodes.putCode(_code, {badge_count: 1}); // put the code in incoming_codes but using a method (in this way we can implement event handling)
+		var mex = '<span class="pull-left" style="padding-top: 11px"><span class="badge">'+RFcodes.incoming_codes[_code].badge_count+'</span> Code detected: '+_code+'</span>'+
 		'<span class="pull-right"><a href="#" class="btn btn-info btn-xs" onclick="events.emit(\'ignoreCode\', '+_code+');">Ignore</a>'+
 		'<a href="#" class="btn btn-success btn-xs" onclick="events.emit(\'assignCode\', '+_code+');">Assign</a></span>';
-		incoming_codes[_code].elem = $.snackbar({
+		RFcodes.incoming_codes[_code].elem = $.snackbar({
 			content: mex,
 		 	timeout: 0,
 		 	htmlAllowed: true,
 		 	onClose: function(){ // when snackbar is closed
-		 		delete incoming_codes[_code];
+		 		// do nothing
 			}}); // print snackbar
 
 	}else{ // RFcode already exists and snackbar too, just increment badge_count field
-		incoming_codes[_code].badge_count++;
-		incoming_codes[_code].elem.find('.badge').fadeOut(500, function() {
-        	$(this).html(incoming_codes[_code].badge_count).fadeIn(500);
+		RFcodes.incoming_codes[_code].badge_count++;
+		RFcodes.incoming_codes[_code].elem.find('.badge').fadeOut(500, function() {
+        	$(this).html(RFcodes.incoming_codes[_code].badge_count).fadeIn(500);
     	});
 	}
 });
