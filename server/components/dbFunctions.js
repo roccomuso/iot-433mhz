06c8f8f97d77c03fe43030d40b5de05a2f126bbc
Rocco Musolino <rocco@hackerstribe.com>
2016-01-15 05:33:46 +0100

    new database functions to support new API for cards submission and retrieving.

--- server/components/dbFunctions.js
+++ server/components/dbFunctions.js
@@ -1,5 +1,5 @@
 
-function initializeRFcodes(data){ // data is the code received through serial
+function _initializeRFcodes(data){ // data is the code received through serial
 	if (typeof data === 'undefined') data = {};
 	// RFcodes table structure
 	return {
@@ -9,6 +9,67 @@ function initializeRFcodes(data){ // data is the code received through serial
 		isIgnored: false, // default values
 		assignedTo: 'none' // attachable to a Device ID
 	};
+}
+
+function _initializeCard(params, img_path){ // params received through API
+	if (typeof img_path === 'undefined') img_path = false;
+	// Card table structure
+	var selected_device = {};
+	if (params.type === 'switch')
+		selected_device = {
+			on_code: parseInt(params.on_code),
+			off_code: parseInt(params.off_code),
+			notification_sound: true,
+			is_on: false
+		};
+	else if (params.type === 'alarm')
+		selected_device = {
+			last_alert: 'no alert yet',
+			notification_sound: true,
+			trigger_code: parseInt(params.trigger_code)
+		};
+	else if (params.type === 'info')
+		selected_device = {};
+
+	return {
+		active: true,
+		headline: params.headline,
+		shortname: params.shortname,
+		card_body: params.card_body,
+		img: img_path, // false if card got no img.
+		room: params.room,
+		type: params.type,
+		device: selected_device
+	};
+}
+
+function _checkDatabaseCorrispondence(db, params){ // check if 
+	// CHECK THAT:
+	// shortname is unique
+	// codes are in db.RFCODES
+	return new Promise(function(resolve, reject){
+		db.CARDS.find({shortname: params.shortname}, function(err, doc){
+			if (err) return reject(err);
+			if (doc.length !== 0) reject('shortname already exists in DB, choose another one.');
+			else{
+				// check for codes
+				var query = {};
+				if (params.type === 'switch') query = { $or: [{ code: parseInt(params.on_code), isIgnored: false, assignedTo: 'none' }, { code: parseInt(params.off_code), isIgnored: false, assignedTo: 'none' }] };
+				else if (params.type === 'alarm') query = {code: parseInt(params.trigger_code), isIgnored: false, assignedTo: 'none'};
+				else if (params.type === 'info') query = {};
+				
+				db.RFCODES.find(query, function(err, docs){
+					var len = (params.type === 'switch') ? 2 : 1 ; // should have found 2 records in codes' DB for switch type
+					if (params.type === 'info') return resolve(true);
+
+					if (docs.length === len)
+						resolve(true);
+					else 
+						reject('Make sure to assign free not Ignored existing codes.');
+				});
+			}
+		});
+	});
 }
 
 module.exports = function(db, config){
@@ -25,7 +86,7 @@ module.exports = function(db, config){
 							resolve('Code already exists in DB');
 						}else{
 							// put it
-							db.RFCODES.insert(initializeRFcodes(data), function(err, newDoc){
+							db.RFCODES.insert(_initializeRFcodes(data), function(err, newDoc){
 								if (err) return reject(err);
 								resolve(newDoc);
 							});
@@ -36,6 +97,15 @@ module.exports = function(db, config){
 			isCodeIgnored: function(code){
 				return new Promise(function(resolve, reject){
 					db.RFCODES.find({code: code, isIgnored: true}, function(err, doc){
+						if (err) return reject(err);
+						if (doc.length === 0) resolve(false);
+						else resolve(true);
+					});
+				});
+			},
+			isCodeAvailable: function(code){
+				return new Promise(function(resolve, reject){
+					db.RFCODES.find({code: code, isIgnored: false, assignedTo: 'none'}, function(err, doc){
 						if (err) return reject(err);
 						if (doc.length === 0) resolve(false);
 						else resolve(true);
@@ -67,6 +137,42 @@ module.exports = function(db, config){
 						resolve(docs);
 					});
 				});
+			},
+			getAllCards: function(){
+				return new Promise(function(resolve, reject){
+					db.CARDS.find({}, function(err, docs){
+						if (err) return reject(err);
+						resolve(docs);
+					});
+				});
+			},			
+			putCardInDatabase: function(req, destination_path){
+				// req.body e req.file
+				return new Promise(function(resolve, reject){
+					
+					_checkDatabaseCorrispondence(db, req.body).then(function(){
+						// normalize 'room' text before DB insertion
+						db.CARDS.insert(_initializeCard(req.body, destination_path), function(err, newDoc){
+								if (err) return reject(err);
+								// set as assigned the codes in DB (handles both 'swtich' and 'alarm' type)
+								db.RFCODES.update({ $or: [{ code: parseInt(req.body.on_code) }, { code: parseInt(req.body.off_code) }, {code: parseInt(req.body.trigger_code)}] }, { $set: { assignedTo: req.body.shortname } }, { multi: true }, function (err, numReplaced) {
+								  // numReplaced = 2
+								  resolve(newDoc); // return the new Card just inserted
+								});
+								
+						});
+
+					}).catch(function(err){
+						reject(err);
+					});
+
+				});
+			},
+			getInitCards: function(){
+				// TODO
+				// cards che vengono renderizzate nella UI
+				return {prova: true, come: 'va'};
+
 			}
 	};
 
