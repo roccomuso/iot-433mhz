7bab025c5b230aaed8b0cb67894be05414dc4f10
Rocco Musolino <rocco@hackerstribe.com>
2016-01-23 03:53:08 +0100

    console mirroring updated.

--- server/www/dist/js/browser.console.mirror.js
+++ server/www/dist/js/browser.console.mirror.js
@@ -15,26 +15,31 @@ var consoleMirroring = (function(document){ // Module Pattern Design
 			if (typeof options.socketLib.io === 'undefined') return reject('Socket.io instance not valid');
 			if (typeof options.containerId === 'undefined') return reject('containerId required.');
 			if (document.getElementById(options.containerId) === null) return reject('containerId not found in the DOM');
+			// set default values
+			if (typeof options.fullScreen === 'undefined') options.fullScreen = false;
+			if (typeof options.border === 'undefined') options.border = true;
+
 			resolve();
 		});
 	}
 
-	function addStyling(elem){
+	function addStyling(elem, extra){
+
 		elem.style.background = '#000';
-  		elem.style.border = '3px groove #ccc';
+  		elem.style.border = (extra.border) ? '3px groove #ccc' : 'none';
   		elem.style.color = '#ccc';
  		elem.style.display = 'block';
-  		elem.style.padding = '5px';
-  		elem.style.width = '70%';
-  		elem.style['min-height'] = '300px';
+  		elem.style.padding = '7px';
+  		elem.style.width = (extra.fullScreen) ? '100%' : '70%';
+  		elem.style.height = (extra.fullScreen) ? '100%' : '300px';
   		elem.style['overflow-y'] = 'scroll';
 	}
 
 	methods.init = function(options){
-		
+
 		validateOptions(options).then(function(){
 			domElem  = document.getElementById(options.containerId);
-			addStyling(domElem);
+			addStyling(domElem, options);
 			var io = options.socketLib;
 			io.on('logEvent', function(data){
 				// {text: '...', color: '#...'} received through socket
