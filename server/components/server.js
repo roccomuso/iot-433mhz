d89b7f0aa61839fae2452f1f278a98080c1a5fb8
Rocco Musolino <rocco@hackerstribe.com>
2016-01-23 17:30:19 +0100

    Print local IP address or addresses (if on multi-interface machines).

--- server/components/server.js
+++ server/components/server.js
@@ -6,7 +6,7 @@ var config = require('../config.json'); // config file
 
 server.listen(config.server_port);
 
-console.log('Server started on port', config.server_port);
+console.info('Server started on', getLocalIPAddress(), '- Port', config.server_port);
 
 module.exports = function(server_cb, socket_cb){
   
@@ -17,3 +17,22 @@ module.exports = function(server_cb, socket_cb){
   socket_cb(io);
 
 };
+
+function getLocalIPAddress() {
+  // synchronous method
+  var interfaces = require('os').networkInterfaces();
+  var IPs = [];
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        IPs.push(alias.address);
+    }
+  }
+
+  if (IPs.length === 1) return IPs[0];
+  else if(IPs.length > 1) return IPs.toString();
+  else return '0.0.0.0';
+}
